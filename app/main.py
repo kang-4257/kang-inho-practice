053db6bb1b41c7690b91d652679cd3377f62ccc9
user <user@k3s-master-node.myguest.virtualbox.org>
2026-03-07 12:04:50 +0900

    fix: 템플릿 경로를 도커 절대 경로(/app)로 고정

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 app = FastAPI()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = "/app"
 
 app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
